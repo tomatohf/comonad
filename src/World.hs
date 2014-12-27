23c56ec174fa5f8caec829dbaa9aff5bada5d305
Tomato <Tomato.HF@gmail.com>
2014-12-27 18:10:52 +0800

    World is a ListZipper of ListZipper(s)

---
+++ src/World.hs
@@ -0,0 +1,35 @@
+module World where
+
+import Zipper
+import Comonad
+
+type GetWorld a = ListZipper (ListZipper a)
+newtype World a = World { getWorld :: GetWorld a }
+
+mapWorld :: (GetWorld a -> GetWorld b) -> World a -> World b
+mapWorld f = World . f . getWorld
+
+up :: World a -> World a
+up = mapWorld back
+
+down :: World a -> World a
+down = mapWorld forward
+
+left :: World a -> World a
+left = mapWorld $ fmap back
+
+right :: World a -> World a
+right = mapWorld $ fmap forward
+
+instance Functor World where
+    fmap = mapWorld . fmap . fmap
+
+instance Comonad World where
+    extract = extract . extract . getWorld
+    duplicate = World . hor . ver
+      where
+        hor = fmap $ zip left right
+        ver = zip up down
+        zip l r a = Zip (shift l) a (shift r)
+          where
+            shift = tail . (flip iterate a)
--- src/Zipper.hs
+++ src/Zipper.hs
@@ -4,14 +4,11 @@ import Comonad
 
 data ListZipper a = Zip [a] a [a]
 
-left :: ListZipper a -> ListZipper a
-left (Zip (l:ls) a rs) = Zip ls l (a:rs)
+back :: ListZipper a -> ListZipper a
+back (Zip (l:ls) a rs) = Zip ls l (a:rs)
 
-right :: ListZipper a -> ListZipper a
-right (Zip ls a (r:rs)) = Zip (a:ls) r rs
-
-get :: ListZipper a -> a
-get (Zip _ a _) = a
+forward :: ListZipper a -> ListZipper a
+forward (Zip ls a (r:rs)) = Zip (a:ls) r rs
 
 list :: ListZipper a -> Int -> [a]
 list (Zip ls a rs) n = reverse (take n ls) ++ a:(take n rs)
@@ -20,6 +17,6 @@ instance Functor ListZipper where
     fmap f (Zip ls a rs) = Zip (fmap f ls) (f a) (fmap f rs)
 
 instance Comonad ListZipper where
-    extract = get
-    duplicate a = Zip (shift left) a (shift right)
+    extract (Zip _ a _) = a
+    duplicate a = Zip (shift back) a (shift forward)
         where shift move = tail $ iterate move a
